c618741d753fa758bc7ab24c54a5377f2fcbf2bb
zhid0399123 <zhid0399123@gmail.com>
2023-11-26 18:51:05 +0300

    feat(server): endpoint versioning and implimented server startup logs

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const compression = require("compression")
-const cors = require("cors")
-const router = require("./routes")
-const routeNotFound = require("./middleware/routeNotFound")
+const express = require('express')
+const dotenv = require('dotenv')
+const compression = require('compression')
+const cors = require('cors')
+const router = require('./routes')
+const pkg = require('../package.json')
+const routeNotFound = require('./middleware/routeNotFound')
+const serverStartUpLog = require('./config/serverStartUpLog')
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(compression())
-app.use("/v1/datestamp/", router)
-app.get("/", async function (req, res) {
+app.use('/v0/api/datestamp/', router)
+app.get('/', async function (req, res) {
   res.json({ root: true })
 })
-
 app.use(routeNotFound)
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server listening on port ${process.env.PORT || 3000}`)
-})
+const port = process.env.PORT || 3000
+app.listen(port, () => serverStartUpLog(port, pkg))
