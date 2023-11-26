ad731e5b6f7b822a40728701f5b2dcdc888089f4
zhid0399123 <zhid0399123@gmail.com>
2023-11-26 18:44:28 +0300

    chore(errorHandler): updated errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,3 +1,6 @@
 module.exports = (error, req, res, next) => {
+  if (error.message === 'Invalid date') {
+    res.statusCode(400).json({ error: error.message, statusCode: 400 })
+  }
   next(error)
 }
