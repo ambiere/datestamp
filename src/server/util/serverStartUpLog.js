191794745cba070eb081f5422951b3e74c063ab1
zhid0399123 <zhid0399123@gmail.com>
2023-11-29 21:17:44 +0300

    feat(serverStartUpLog): modified logger util fn to receive app address instead of port

--- src/server/util/serverStartUpLog.js
+++ src/server/util/serverStartUpLog.js
@@ -1,12 +1,10 @@
 const { gray, cyan } = require('kolorist')
 
-module.exports = (port, pkg) => {
+module.exports = (address, pkg) => {
   console.log('\u001b[32m[server]', 'up and running\u001b[0m\n')
-  console.log(gray(`datestamp npm version: ${pkg.version}\n`))
+  console.log(gray(`datestamp microservice version: ${pkg.version}\n`))
   console.log(cyan("\\(॰ ꤮ ॰)/ What's up there?"))
   console.log(gray('\nHome'))
-  console.log(`http://localhost:${port}/v${pkg.version.at(0)}/api`)
-  console.log(gray('\nBaseUrl'))
-  console.log(`http://localhost:${port}/v${pkg.version.at(0)}/api/datestamp`)
+  console.log(`http://${address.address}:${address.port}/v${pkg.version.at(0)}/api/datestamp`)
   console.log(gray('...'))
 }
