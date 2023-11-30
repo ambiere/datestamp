4b5072ba509d49b5766bd9408040db0ff9e7ab0e
zhid0399123 <zhid0399123@gmail.com>
2023-11-30 22:30:32 +0300

    feat(index): added datestamp/ route to handle request when empty date param is passed

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,15 +2,20 @@
 
 const { Router, json } = require('express')
 const pinoHttp = require('pino-http')
+const path = require('path')
 const loggerOptions = require('../config/loggerOptions')
 const toUnix = require('../util/toUnix')
 const toUTC = require('../util/toUTC')
-const router = Router()
+const router = Router({ strict: true })
 
 router.use(json())
 router.use(pinoHttp({ ...loggerOptions }))
 
-router.get('/', async function (req, res, next) {
+router.get('/datestamp', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../../../', 'public/'))
+})
+
+router.get('/datestamp/', async function (req, res, next) {
   try {
     const date = new Date()
     res.json({ ...toUnix(date), ...toUTC(date) })
@@ -19,7 +24,7 @@ router.get('/', async function (req, res, next) {
   }
 })
 
-router.get('/:date', async function (req, res, next) {
+router.get('/datestamp/:date', async function (req, res, next) {
   const dateParam = req.params.date
   let date = {}
   isNaN(dateParam) ? (date = new Date(dateParam)) : (date = new Date(parseInt(dateParam)))
